683c506d1514e3f24a52a55616f01539e643012e
wanny95 <wankims@gmail.com>
2025-04-16 15:46:50 +0900

    feature: Implementation SSD write success and fail
    Signed-off-by: wanny95 <wankims@gmail.com>

--- SSD/ssd.cpp
+++ SSD/ssd.cpp
@@ -13,3 +13,26 @@ SSD::Read(unsigned int nLba)
 
     return 0;
 }
+
+unsigned int SSD::write(unsigned int nLba, std::string value)
+{
+    if (nLba < MIN_LBA || nLba > MAX_LBA)
+    {
+        return 1;
+    }
+
+    std::string valueHeader = value.substr(0, 2);
+    std::string valueBody = value.substr(2);
+
+    if (valueHeader != "0x" && valueHeader != "0X")
+        return 1;
+
+    for (auto str : valueBody) {
+        if (isdigit(str)) continue;
+        if (str >= 'A' && str <= 'F') continue;
+        if (str >= 'a' && str <= 'f') continue;
+        return 1;
+    }
+
+    return 0;
+}
--- SSD/ssd.h
+++ SSD/ssd.h
@@ -4,10 +4,15 @@
 #define MIN_LBA (0)
 #define MAX_LBA (99)
 
+#include <string>
+
+using std::string;
+
 class SSD
 {
 public:
     unsigned int Read(unsigned int nLba);
+    unsigned int write(unsigned int nLba, string value);
 private:
 };
 
