63708d9ee9bdf3fb9236a0cc32a66dff951f2b86
Jonghwi <ellia2431@naver.com>
2025-04-16 14:07:16 +0900

    tc: add testcases for read command
    Signed-off-by: Jonghwi <ellia2431@naver.com>

--- SSD/main.cpp
+++ SSD/main.cpp
@@ -1,5 +1,27 @@
 #include "gmock/gmock.h"
 
+TEST(SSDTest, readSuccess) {
+	SSD ssd;
+	uint32_t lba = 0;
+	uint32_t expected = 0x00000000;
+
+	uint32_t actual = ssd.read(lba);
+	EXPECT_EQ(expected, actual);
+}
+
+TEST(SSDTest, readFailWithInvalidLBA) {
+	SSD ssd;
+	uint32_t lba = 100;
+
+	try {
+		ssd.read(lba);
+		FAIL();
+	}
+	catch (exception& e) {
+		EXPECT_EQ(string{ e.what() }, string{ "Out of LBA range" });
+	}
+}
+
 int main(void) {
 	::testing::InitGoogleMock();
 	return RUN_ALL_TESTS();
