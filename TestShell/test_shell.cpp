0ecb496e292911bf3aac5fe40a167a46107ca9e1
Hyeonseung Yu <hyeonseung.yu@gmail.com>
2025-04-16 16:28:13 +0900

    refactoring: validator extraction and add constants

--- TestShell/test_shell.cpp
+++ TestShell/test_shell.cpp
@@ -6,29 +6,35 @@ public:
 	TestShell(SSD* ssd) : ssd(ssd) {}
 
 	string read(uint32_t address) {
-		if (address > 99 || address < 0)
-			return "ERROR";
+		if (!isValidAddress(address)) return "ERROR";
 		return ssd->read(address);
 	}
 
 	string write(uint32_t address, string value) {
-		if (address > 99 || address < 0)
-			return "ERROR";
-
-		if (value.length() == 10 && value[0] == '0' && value[1] == 'x') {
-			for (int i = 2; i < 10; i++) {
-				if ((value[i] >= '0' && value[i] <= '9') || (value[i] >= 'A' && value[i] <= 'F')) continue;
-
-				return "ERROR";
-			}
-		}
-		else {
-			return "ERROR";
-		}
+		if (!isValidAddress(address)) return "ERROR";
+		if (!isValidValue(value)) return "ERROR";
 
 		ssd->write(address, value);
 		return "Done";
 	}
 private:
 	SSD* ssd;
+
+	bool isValidAddress(uint32_t address) {
+		if (address > 99 || address < 0) return false;
+
+		return true;
+	}
+
+	bool isValidValue(string value) {
+		if (value.length() != 10 || value[0] != '0' || value[1] != 'x')
+			return false;
+
+		for (int i = 2; i < 10; i++) {
+			if ((value[i] >= '0' && value[i] <= '9') || (value[i] >= 'A' && value[i] <= 'F')) continue;
+			return false;
+		}
+
+		return true;
+	}
 };
