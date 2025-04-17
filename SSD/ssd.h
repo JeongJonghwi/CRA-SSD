d9e9c42b8ac700ba7e2def7c08ded9358354c66c
sechakb <dongri.kang@samsung.com>
2025-04-17 09:16:51 +0900

    refactoring: move functions to ssd class
    Signed-off-by: sechakb <dongri.kang@samsung.com>

--- SSD/ssd.h
+++ SSD/ssd.h
@@ -3,11 +3,16 @@
 
 #define MIN_LBA (0)
 #define MAX_LBA (99)
+#define VALUE_INPUT_LENGTH (10)
+#define OUT
 
 #include <string>
 #include <stdint.h>
+#include <cstring>
+#include <cstdlib>
 
 using std::string;
+using std::strtol;
 
 class SSD
 {
@@ -17,7 +22,12 @@ public:
     uint32_t Write(uint32_t nLba, uint32_t value);
     static string getSsdNandFileName();
     static string getSsdOutputFileName();
+    static bool isValidCheckAndCastType(int argc, char* argv[], OUT unsigned int* pnLba, OUT unsigned int* pnValue);
 private:
+    static bool CheckCMDandNumofParam(int argc, char* argv[]);
+    static bool CheckLBA(int argc, char* argv[], OUT unsigned int* pnLba);
+    static bool CheckValue(int argc, char* argv[], OUT unsigned int* pnValue);
+
     static const std::string nandFileName;
     static const std::string nandOutputFileName;
 };
