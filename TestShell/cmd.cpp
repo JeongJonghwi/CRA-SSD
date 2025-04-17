7d1f9e20ac883c36d6a15f5da5fc4d98e345d725
Jonghwi <ellia2431@naver.com>
2025-04-17 15:50:45 +0900

    refactor: split test codes to a file
    Signed-off-by: Jonghwi <ellia2431@naver.com>

--- TestShell/cmd.cpp
+++ TestShell/cmd.cpp
@@ -1,134 +1,140 @@
+#include <sstream>
 #include <string>
 #include <vector>
-#include <sstream>
-#include <iostream>
+
 using std::string;
 using std::vector;
 
 class CMD {
 public:
-	CMD() {};
+    bool validCheck(string command)
+    {
+        vector<string> rawdata = splitBySpace(command);
 
-	bool validCheck(string command) {
+        if (rawdata.size() <= 0 || rawdata.size() > 3)
+            return false;
 
-		vector<string> rawdata = splitBySpace(command);
+        input = rawdata[0];
 
-		if (rawdata.size() <= 0 || rawdata.size() > 3)
-			return false;
+        if (rawdata.size() == 3) {
+            if (input != "write" || !isNumber(rawdata[1])) {
+                address = -1;
+                return false;
+            }
+            if (!isValidAddress(stoi(rawdata[1])))
+                return false;
+            address = stoi(rawdata[1]);
 
-		input = rawdata[0];
+            if (!isValidValue(rawdata[2]))
+                return false;
+            data = rawdata[2];
+            return true;
+        }
 
-		if (rawdata.size() == 3) {
-			if (input != "write" || !isNumber(rawdata[1])) {
-				address = -1;
-				return false;
-			}
-			if (!isValidAddress(stoi(rawdata[1])))
-				return false;
-			address = stoi(rawdata[1]);
-			
-			if (!isValidValue(rawdata[2]))
-				return false;
-			data = rawdata[2];
-			return true;
-		}
+        if (rawdata.size() == 2) {
+            if (input != string("read") && input != "fullwrite")
+                return false;
 
-		if (rawdata.size() == 2) {
+            if (input == "read") {
+                if (!isNumber(rawdata[1]))
+                    return false;
+                if (!isValidAddress(stoi(rawdata[1])))
+                    return false;
 
-			if (input != string("read") && input != "fullwrite")
-				return false;
+                address = stoi(rawdata[1]);
 
-			if (input == "read") {
-				if (!isNumber(rawdata[1])) return false;
-				if (!isValidAddress(stoi(rawdata[1])))
-					return false;
+                return true;
+            }
 
-				address = stoi(rawdata[1]);
+            if (!isValidValue(rawdata[1]))
+                return false;
+            data = rawdata[1];
+            return true;
+        }
 
-				return true;
-			}
+        if (input == "help" || input == "exit" || input == "fullread")
+            return true;
 
-			if (!isValidValue(rawdata[1]))
-				return false;
-			data = rawdata[1];
-			return true;
-		}
+        if (isTestScript(input))
+            return true;
 
-		if (input == "help" || input == "exit" || input == "fullread")
-			return true;
+        return false;
+    }
 
-		if (isTestScript(input)) {
-			return true;
-		}
+    string getCommand()
+    {
+        return input;
+    }
 
-		return false;
-	}
+    int getAddress()
+    {
+        return address;
+    }
 
-	string getCommand() {
-		return input;
-	}
-
-	int getAddress() {
-		return address;
-	}
-
-	string getValue() {
-		return data;
-	}
+    string getValue()
+    {
+        return data;
+    }
 
 private:
-	string input;
-	int address;
-	string data;
+    string input;
+    int address;
+    string data;
 
-	bool isValidAddress(uint32_t address) {
-		if (address > 99 || address < 0) return false;
-		return true;
-	}
+    bool isValidAddress(uint32_t address)
+    {
+        if (address > 99 || address < 0)
+            return false;
+        return true;
+    }
 
-	bool isValidValue(string value) {
-		if (value.length() != 10 || value[0] != '0' || value[1] != 'x')
-			return false;
+    bool isValidValue(string value)
+    {
+        if (value.length() != 10 || value[0] != '0' || value[1] != 'x')
+            return false;
 
-		for (int i = 2; i < 10; i++) {
-			if ((value[i] >= '0' && value[i] <= '9') || (value[i] >= 'A' && value[i] <= 'F') || 
-				(value[i] >= 'a' && value[i] <= 'f')) continue;
-			return false;
-		}
-		return true;
-	}
+        for (int i = 2; i < 10; i++) {
+            if ((value[i] >= '0' && value[i] <= '9') || (value[i] >= 'A' && value[i] <= 'F') || (value[i] >= 'a' && value[i] <= 'f'))
+                continue;
+            return false;
+        }
+        return true;
+    }
 
-	vector<string> splitBySpace(const string& input) {
-		std::istringstream iss(input);
-		std::string word;
-		std::vector<string> result;
+    vector<string> splitBySpace(const string& input)
+    {
+        std::istringstream iss(input);
+        std::string word;
+        std::vector<string> result;
 
-		while (iss >> word) {
-			result.push_back(word);
-		}
-		return result;
-	}
+        while (iss >> word) {
+            result.push_back(word);
+        }
+        return result;
+    }
 
-	bool isNumber(string s) {
-		if (s.size() > 2) return false;
+    bool isNumber(string s)
+    {
+        if (s.size() > 2)
+            return false;
 
-		for (auto c : s) {
-			if (c < '0' || c > '9')
-				return false;
-		}
-		return true;
-	}
+        for (auto c : s) {
+            if (c < '0' || c > '9')
+                return false;
+        }
+        return true;
+    }
 
-	bool isTestScript(string s) {
-		if (s == "1_" || s == "2_" || s == "3_") {
-			return true;
-		}
+    bool isTestScript(string s)
+    {
+        if (s == "1_" || s == "2_" || s == "3_")
+            return true;
 
-		if (s == "1_FullWriteAndReadCompare"
-			|| s == "2_PartialLBAWrite"
-			|| s == "3_WriteReadAging")
-			return true;
+        if (s == "1_FullWriteAndReadCompare"
+            || s == "2_PartialLBAWrite"
+            || s == "3_WriteReadAging")
+            return true;
 
-		return false;
-	}
+        return false;
+    }
 };
--- TestShell/test_shell.cpp
+++ TestShell/test_shell.cpp
@@ -1,145 +1,166 @@
-#include <vector>
+#include "cmd.cpp"
 #include "interface.h"
 #include "gmock/gmock.h"
-#include "cmd.cpp"
+#include <vector>
 
 class TestShell {
 public:
-	TestShell(SSD* ssd) : ssd(ssd) {}
+    TestShell(SSD* ssd)
+        : ssd(ssd)
+    {
+    }
 
-	string read(uint32_t address) {
-		if (!isValidAddress(address)) return "ERROR";
-		return ssd->read(address);
-	}
+    string read(uint32_t address)
+    {
+        if (!isValidAddress(address))
+            return "ERROR";
+        return ssd->read(address);
+    }
 
-	string write(uint32_t address, string value) {
-		if (!isValidAddress(address)) return "ERROR";
-		if (!isValidValue(value)) return "ERROR";
+    string write(uint32_t address, string value)
+    {
+        if (!isValidAddress(address))
+            return "ERROR";
+        if (!isValidValue(value))
+            return "ERROR";
 
-		ssd->write(address, value);
-		return "Done";
-	}
+        ssd->write(address, value);
+        return "Done";
+    }
 
-	string help() {
-		string ret = "Team CodeCraft: ";
-		ret += "°­µ¿Çö, ±èÅÂ¿Ï, À¯Çö½Â, ÀÌ¼öÁö, Á¤Á¾ÈÖ\n";
-		ret += "write: write {value} to {address}\n";
-		ret += "read: read from {address}\n";
-		ret += "fullwrite: write {value} to 0x0 ~ 0x100\n";
-		ret += "fullread: read from all 0x0 ~ 0x100\n";
-		ret += "help: print this message\n";
-		ret += "exit: exit TestShell\n";
-		return ret;
-	}
+    string help()
+    {
+        string ret = "Team CodeCraft: ";
+        ret += "°­µ¿Çö, ±èÅÂ¿Ï, À¯Çö½Â, ÀÌ¼öÁö, Á¤Á¾ÈÖ\n";
+        ret += "write: write {value} to {address}\n";
+        ret += "read: read from {address}\n";
+        ret += "fullwrite: write {value} to 0x0 ~ 0x100\n";
+        ret += "fullread: read from all 0x0 ~ 0x100\n";
+        ret += "help: print this message\n";
+        ret += "exit: exit TestShell\n";
+        return ret;
+    }
 
-	string exit() {
-		return "Exit TestShell";
-	}
+    string exit()
+    {
+        return "Exit TestShell";
+    }
 
-	string fullWrite(string value) {
-		if (!isValidValue(value)) return "ERROR";
-		for (int i = SSD_MINIMUM_ADDRESS; i < SSD_MAXIMUM_ADDRESS; i++) {
-			ssd->write(i, value);
-		}
-		return "Done";
-	}
+    string fullWrite(string value)
+    {
+        if (!isValidValue(value))
+            return "ERROR";
+        for (int i = SSD_MINIMUM_ADDRESS; i < SSD_MAXIMUM_ADDRESS; i++) {
+            ssd->write(i, value);
+        }
+        return "Done";
+    }
 
-	string fullRead() {
-		string ret = "";
-		for (int i = SSD_MINIMUM_ADDRESS; i < SSD_MAXIMUM_ADDRESS; i++) {
-			ret += ssd->read(i); 
-			ret += "\n";
-		}
-		return ret;
-	}
+    string fullRead()
+    {
+        string ret = "";
+        for (int i = SSD_MINIMUM_ADDRESS; i < SSD_MAXIMUM_ADDRESS; i++) {
+            ret += ssd->read(i);
+            ret += "\n";
+        }
+        return ret;
+    }
 
-	string fullWriteAndReadCompare() {
+    string fullWriteAndReadCompare()
+    {
+        for (int groupStart = SSD_MINIMUM_ADDRESS; groupStart < SSD_MAXIMUM_ADDRESS; groupStart += 5) {
+            std::stringstream ss;
+            ss << std::hex << std::setw(2) << std::setfill('0') << groupStart;
+            string value = "0x123456" + ss.str();
+            if (groupWriteAndReadCompare(groupStart, groupStart + 5, value) == "FAIL")
+                return "FAIL";
+        }
 
-		for (int groupStart = SSD_MINIMUM_ADDRESS; groupStart < SSD_MAXIMUM_ADDRESS; groupStart += 5) {
-			std::stringstream ss;
-			ss << std::hex << std::setw(2) << std::setfill('0') << groupStart;
-			string value = "0x123456" + ss.str();
-			if (groupWriteAndReadCompare(groupStart, groupStart + 5, value) == "FAIL") return "FAIL";
-		}
+        return "PASS";
+    }
 
-		return "PASS";
-	}
+    string partialLBAWrite(string value)
+    {
+        if (!isValidValue(value))
+            return "FAIL";
 
-	string partialLBAWrite(string value) {
-		if (!isValidValue(value)) return "FAIL";
+        std::vector<int> order = { 4, 0, 3, 1, 2 };
 
-		std::vector<int> order = { 4,0,3,1,2 };
-		
-		for (int i = 0; i < 30; i++) {
-			for (auto o : order) {
-				ssd->write(o, value);
-			}
+        for (int i = 0; i < 30; i++) {
+            for (auto o : order) {
+                ssd->write(o, value);
+            }
 
-			for (auto o : order) {
-				if (didReadFail(ssd->read(o), value)) return "FAIL";
-			}
-		}
+            for (auto o : order) {
+                if (didReadFail(ssd->read(o), value))
+                    return "FAIL";
+            }
+        }
+        return "PASS";
+    }
 
-		return "PASS";
-	}
+    string writeReadAging(string value)
+    {
+        if (!isValidValue(value))
+            return "FAIL";
 
-	string writeReadAging(string value) {
-		if (!isValidValue(value)) return "FAIL";
+        for (int i = 0; i < 200; i++) {
+            ssd->write(0, value);
+            ssd->write(99, value);
+            if (didReadFail(ssd->read(0), ssd->read(99)))
+                return "FAIL";
+        }
+        return "PASS";
+    }
 
-		for (int i = 0; i < 200; i++)
-		{
-			ssd->write(0, value);
-			ssd->write(99, value);
-			if (didReadFail(ssd->read(0), ssd->read(99)))
-				return "FAIL";
-		}
-		return "PASS";
-	}
+    string groupWriteAndReadCompare(int startAddr, int endAddr, string value)
+    {
+        for (int addr = startAddr; addr < endAddr; addr++) {
+            ssd->write(addr, value);
+        }
 
-	string groupWriteAndReadCompare(int startAddr, int endAddr, string value) {
+        for (int addr = startAddr; addr < endAddr; addr++) {
+            if (didReadFail(ssd->read(addr), value))
+                return "FAIL";
+        }
+        return "PASS";
+    }
 
-		for (int addr = startAddr; addr < endAddr; addr++) {
-			ssd->write(addr, value);
-		}
-
-		for (int addr = startAddr; addr < endAddr; addr++) {
-			if (didReadFail(ssd->read(addr), value)) return "FAIL";
-		}
-
-		return "PASS";
-	}
-
-	bool isValidCommandAndArgument(string command) {
-		CMD cmd;
-		return cmd.validCheck(command);
-	}
+    bool isValidCommandAndArgument(string command)
+    {
+        CMD cmd;
+        return cmd.validCheck(command);
+    }
 
 private:
-	SSD* ssd;
-	const int SSD_MINIMUM_ADDRESS = 0;
-	const int SSD_MAXIMUM_ADDRESS = 100;
+    SSD* ssd;
+    const int SSD_MINIMUM_ADDRESS = 0;
+    const int SSD_MAXIMUM_ADDRESS = 100;
 
-	bool didReadFail(string actual, string expected) {
-		if (actual != expected) return true;
-		return false;
-	}
+    bool didReadFail(string actual, string expected)
+    {
+        if (actual != expected)
+            return true;
+        return false;
+    }
 
-	bool isValidAddress(uint32_t address) {
-		if (address > 99 || address < 0) return false;
+    bool isValidAddress(uint32_t address)
+    {
+        if (address > 99 || address < 0)
+            return false;
+        return true;
+    }
 
-		return true;
-	}
+    bool isValidValue(string value)
+    {
+        if (value.length() != 10 || value[0] != '0' || value[1] != 'x')
+            return false;
 
-	bool isValidValue(string value) {
-		if (value.length() != 10 || value[0] != '0' || value[1] != 'x')
-			return false;
-
-		for (int i = 2; i < 10; i++) {
-			if ((value[i] >= '0' && value[i] <= '9') || (value[i] >= 'A' && value[i] <= 'F') ||
-				(value[i] >= 'a' && value[i] <= 'f')) continue;
-			return false;
-		}
-
-		return true;
-	}
+        for (int i = 2; i < 10; i++) {
+            if ((value[i] >= '0' && value[i] <= '9') || (value[i] >= 'A' && value[i] <= 'F') || (value[i] >= 'a' && value[i] <= 'f'))
+                continue;
+            return false;
+        }
+        return true;
+    }
 };
