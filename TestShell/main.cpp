0abd8d712583ecc1aba9b3f8f2a6a79db56d9f73
Hyeonseung Yu <hyeonseung.yu@gmail.com>
2025-04-17 14:30:12 +0900

    feature: testshell run func

--- TestShell/main.cpp
+++ TestShell/main.cpp
@@ -253,6 +253,51 @@ TEST_F(SSDFixture, writeWithRealSSD) {
 }
 
 int main(void) {
+
+	RealSSD ssd;
+	TestShell test_shell(&ssd);
+
+	while(1){
+		string input;
+		std::cout << "Shell > ";
+		std::getline(std::cin, input);
+
+		CMD cmd;
+		if (cmd.validCheck(input)) {
+			string command = cmd.getCommand();
+
+			if (command == "read") {
+				std::cout << test_shell.read(cmd.getAddress()) << std::endl;
+			}
+			else if (command == "write") {
+				test_shell.write(cmd.getAddress(), cmd.getValue());
+			}
+			else if (command == "fullread") {
+				std::cout << test_shell.fullRead() << std::endl;
+			}
+			else if (command == "fullwrite") {
+				test_shell.fullWrite(cmd.getValue());
+			}
+			else if (command == "help") {
+				std::cout << test_shell.help() << std::endl;
+			}
+			else if (command == "exit") {
+				return 0;
+			}
+			else if (command == "1_" || command == "1_FullWriteAndReadCompare") {
+				std::cout << test_shell.fullWriteAndReadCompare() << std::endl;
+			}
+			else if (command == "2_" || command == "2_PartialLBAWrite") {
+				std::cout << test_shell.partialLBAWrite(cmd.getValue()) << std::endl;
+			}
+			else if (command == "3_" || command == "3_WriteReadAging") {
+				std::cout << test_shell.writeReadAging(cmd.getValue()) << std::endl;
+			}
+		}
+		else
+			std::cout << "INVALID COMMAND" << std::endl;
+	}
+	
 	::testing::InitGoogleMock();
 	return RUN_ALL_TESTS();
 }
