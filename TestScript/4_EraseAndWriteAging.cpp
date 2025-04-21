7cfc1f4d7cde9544cedab1cedd321a2ef8e648c2
Hyeonseung Yu <hyeonseung.yu@gmail.com>
2025-04-21 09:57:28 +0900

    feature: dll testscripts implemented

--- TestScript/4_EraseAndWriteAging.cpp
+++ TestScript/4_EraseAndWriteAging.cpp
@@ -3,35 +3,37 @@
 #include "test_script_interface.h"
 
 #include <iostream>
-#include <algorithm>
+#include <algorithm>    
 #include <vector>
 
-class PartialWriteAndReadCompareScript : public ITestScript {
+class EraseAndWriteAging : public ITestScript
+{
 public:
-    PartialWriteAndReadCompareScript(SSD* ssd)
+    EraseAndWriteAging(SSD* ssd)
         : ssd(ssd)
-    {   
+    {
     }
 
     string Run() override
     {
-        string value = "0x12345678";
-
-        if (!isValidValue(value))
-            return "FAIL";
-
-        std::vector<int> order = { 4, 0, 3, 1, 2 };
-
+        ssd->erase(0, 3);
         for (int i = 0; i < 30; i++) {
-            for (auto o : order) {
-                ssd->write(o, value);
-            }
+            for (int startAddr = 2; startAddr <= 96; startAddr += 2) {
+                ssd->write(startAddr, "0x12345678");
+                ssd->write(startAddr, "0x87654321");
+                ssd->erase(startAddr, 3);
 
-            for (auto o : order) {
-                if (didReadFail(ssd->read(o), value))
+                if (didReadFail(ssd->read(startAddr), "0x00000000"))
+                    return "FAIL";
+
+                if (didReadFail(ssd->read(startAddr + 1), "0x00000000"))
+                    return "FAIL";
+
+                if (didReadFail(ssd->read(startAddr + 2), "0x00000000"))
                     return "FAIL";
             }
         }
+
         return "PASS";
     }
 
@@ -63,12 +65,12 @@ private:
     }
 };
 
-extern "C" __declspec(dllexport) ITestScript* CreateScript_2_(SSD* ssd)
+extern "C" __declspec(dllexport) ITestScript* CreateScript_4_(SSD* ssd)
 {
-    return new PartialWriteAndReadCompareScript(ssd);
+    return new EraseAndWriteAging(ssd);
 }
 
-extern "C" __declspec(dllexport) ITestScript* CreateScript_2_PartialLBAWrite(SSD* ssd)
+extern "C" __declspec(dllexport) ITestScript* CreateScript_4_EraseAndWriteAging(SSD* ssd)
 {
-    return new PartialWriteAndReadCompareScript(ssd);
+    return new EraseAndWriteAging(ssd);
 }
