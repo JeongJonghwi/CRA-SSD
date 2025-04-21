0bc19ad6c3c931ef35f512dcdf17cea4d92929fc
sechakb <dongri.kang@samsung.com>
2025-04-21 10:28:15 +0900

    feature: CommandBufferManager
    Signed-off-by: sechakb <dongri.kang@samsung.com>

--- SSD/command_buffer_manager.cpp
+++ SSD/command_buffer_manager.cpp
@@ -2,6 +2,10 @@
 #include <windows.h>
 #include <cstdlib>
 #include <io.h>
+#include <algorithm>
+
+using std::min;
+using std::max;
 
 CommandBufferManager::CommandBufferManager()
 {
@@ -25,7 +29,7 @@ bool CommandBufferManager::FastRead(uint32_t lba, uint32_t& readValue)
             }
         }
         else if (command.type == ERASE) {
-            if (lba >= command.lba && lba < command.lba + command.value) {
+            if (lba >= command.lba && lba <= command.GetEnd()) {
                 readValue = 0;
                 return true;
             }
@@ -33,6 +37,46 @@ bool CommandBufferManager::FastRead(uint32_t lba, uint32_t& readValue)
     }
     
     return false;
+}
+
+void CommandBufferManager::AddWrite(uint32_t lba, uint32_t value)
+{
+    for (list<Command>::iterator iter = commands.begin(); iter != commands.end();)
+    {
+        if (iter->lba == lba) {
+            iter = Delete(iter);
+            continue;
+        }
+        iter++;
+    }
+
+    AddCommand(WRITE, lba, value);
+    
+    MergeErase();
+}
+
+void CommandBufferManager::AddErase(uint32_t lba, uint32_t value)
+{
+    for (list<Command>::iterator iter = commands.begin(); iter != commands.end();)
+    {
+        if (iter->type == WRITE) {
+            if ((iter->lba >= lba) && (iter->lba < lba + value)) {
+                iter = Delete(iter);
+                continue;
+            }
+        }
+        else {  // if (iter->type == ERASE)
+            if ((iter->lba >= lba) && (iter->GetEnd() <= lba + value - 1)) {
+                iter = Delete(iter);
+                continue;
+            }
+        }
+        iter++;
+    }
+
+    AddCommand(ERASE, lba, value);
+
+    MergeErase();
 }
 
 bool CommandBufferManager::CheckDirectoryExists(const string& path)
@@ -87,7 +131,7 @@ void CommandBufferManager::ScanFiles()
 
             start = 0;
             end = filename.find('_');
-            command.order = stoi(filename.substr(start, end-start));
+            command.order = static_cast<uint32_t>(stoul(filename.substr(start, end-start)));
 
             start = end + 1;
             end = filename.find('_', start);
@@ -95,11 +139,11 @@ void CommandBufferManager::ScanFiles()
 
             start = end + 1;
             end = filename.find('_', start);
-            command.lba = stoi(filename.substr(start, end-start));
+            command.lba = static_cast<uint32_t>(stoul(filename.substr(start, end-start)));
 
             start = end + 1;
             end = filename.find('_', start);
-            command.value = stoi(filename.substr(start, end-start));
+            command.value = static_cast<uint32_t>(stoul(filename.substr(start, end-start)));    // decimal
         
             commands.push_back(command);
         }
@@ -107,4 +151,103 @@ void CommandBufferManager::ScanFiles()
     _findclose(handle);
 
     commands.sort();
+}
+
+list<Command>::iterator CommandBufferManager::Delete(list<Command>::iterator iter)
+{
+    Invalidate(iter);
+    iter = commands.erase(iter);
+
+    if (iter == commands.begin() || commands.size() == 0) {
+        return iter;
+    }
+
+    list<Command>::iterator it = iter;
+    for (it--; it != commands.begin(); it--) {
+        Rename(it, it->order-1, it->type, it->lba, it->value);
+        it->order--;
+    }
+
+    return iter;
+}
+
+string CommandBufferManager::GetFileName(Command &command)
+{
+    string combined = string(BUFFER_DIRECTORY_NAME) + "\\";
+    combined += to_string(command.order);
+    combined += "_" + cmd_symbol[command.type];
+    combined += "_" + to_string(command.lba);
+    combined += "_" + to_string(command.value); // decimal
+    return combined;
+}
+
+string CommandBufferManager::GetFileName(uint32_t order, CmdType type, uint32_t lba, uint32_t value)
+{
+    string combined = string(BUFFER_DIRECTORY_NAME) + "\\";
+    combined += to_string(order);
+    combined += "_" + cmd_symbol[type];
+    combined += "_" + to_string(lba);
+    combined += "_" + to_string(value);         // decimal
+    return combined;
+}
+
+void CommandBufferManager::Rename(list<Command>::iterator iter, int32_t order, CmdType type, uint32_t lba, uint32_t value)
+{
+    string old = GetFileName(*iter);
+    string changed = GetFileName(order, type, lba, value);
+    rename(old.c_str(), changed.c_str());
+}
+
+void CommandBufferManager::Invalidate(list<Command>::iterator iter)
+{
+    string old = GetFileName(*iter);
+    string changed = string(BUFFER_DIRECTORY_NAME) + "\\" + to_string(valid_count--) + "_empty";
+    rename(old.c_str(), changed.c_str());
+}
+
+void CommandBufferManager::AddCommand(CmdType type, uint32_t lba, uint32_t value)
+{
+    string old = string(BUFFER_DIRECTORY_NAME) + "\\" + to_string(++valid_count) + "_empty";
+    string changed = GetFileName(valid_count, type, lba, value);
+    rename(old.c_str(), changed.c_str());
+    commands.emplace_front(valid_count, lba, value, type);
+}
+
+void CommandBufferManager::MergeErase()
+{
+    for (list<Command>::iterator iter = commands.begin(); iter != commands.end();)
+    {
+        if (iter->type == ERASE) {
+            list<Command>::iterator next = iter;
+            next++;
+            if (next == commands.end() || next->type != ERASE) {
+                break;
+            }
+
+            uint32_t start1 = iter->lba;
+            uint32_t start2 = next->lba;
+            uint32_t end1 = iter->GetEnd();
+            uint32_t end2 = next->GetEnd();
+
+            if (end1 < start2 - 1 || end2 < start1 - 1) {
+                uint32_t new_start = min(start1, start2);
+                uint32_t new_end = max(end1, end2);
+                uint32_t new_size = new_end - new_start + 1;
+                
+                if (new_size > MAX_COUNT) {
+                    iter = next;
+                    continue;
+                }
+                
+                Delete(iter);
+                Rename(next, next->order, next->type, new_start, new_size);
+                next->lba = new_start;
+                next->value = new_size;
+                iter = next;
+                continue;
+            }
+        }
+
+        iter++;
+    }
 }
--- SSD/main.cpp
+++ SSD/main.cpp
@@ -1,7 +1,13 @@
 #include "ssd.h"
 #include "command_buffer_manager.h"
 
-#ifdef _DEBUG
+#define TEST (1)
+#if TEST
+#include <iostream>
+#include <cstring>
+#endif
+
+#ifndef _DEBUG
 #include "gmock/gmock.h"
 
 int main(void)
@@ -12,6 +18,7 @@ int main(void)
 #else
 int main(int argc, char* argv[])
 {
+#if (TEST == 0)
     SSD ssd;
     CmdType cmd;
     CommandBufferManager buffermanager;
@@ -38,7 +45,8 @@ int main(int argc, char* argv[])
     }
     case WRITE:
     {
-        ssd.Write(lba, value);
+        // ssd.Write(lba, value);
+        buffermanager.AddWrite(lba, value);
         break;
     }
     case ERASE:
@@ -46,10 +54,65 @@ int main(int argc, char* argv[])
         if (value == 0) {
             break;
         }
-        ssd.Erase(lba, value);
+        // ssd.Erase(lba, value);
+        buffermanager.AddErase(lba, value);
         break;
     }
     }
+#else
+    argc = 1;
+    char input[5][20];
+    while (true) {
+        std::cout << "input: ";
+        std::cin >> input[argc];
+
+        if (strcmp(input[argc], "exit") == 0)
+            break;
+
+        argv[argc] = input[argc];
+        argc++;
+    }
+    SSD ssd;
+    CmdType cmd;
+    CommandBufferManager buffermanager;
+    uint32_t lba, value;
+
+    if (ssd.ValidCheckAndCastType(argc, argv, &cmd, &lba, &value) == false) {
+        ssd.WriteToOutputFileError();
+        return 0;
+    }
+
+    switch (cmd) {
+    case READ:
+    {
+        uint32_t readValue;
+        if (buffermanager.FastRead(lba, readValue)) {
+            if (ssd.WriteToOutputFile(readValue) == false) {
+                return 0;
+            }
+        }
+        else {
+            ssd.Read(lba);
+        }
+        break;
+    }
+    case WRITE:
+    {
+        // ssd.Write(lba, value);
+        buffermanager.AddWrite(lba, value);
+        break;
+    }
+    case ERASE:
+    {
+        if (value == 0) {
+            break;
+        }
+        // ssd.Erase(lba, value);
+        buffermanager.AddErase(lba, value);
+        break;
+    }
+    }
+#endif
 
     return 0;
 }
