5d13a505c0613119f40971a3505dccfec5d20d51
suji0908.lee <suji0908.lee@samsung.net>
2025-04-21 12:38:58 +0900

    refactoring : create shell.h
    Signed-off-by: suji0908.lee <suji0908.lee@samsung.net>

---
+++ TestShell/shell.h
@@ -0,0 +1,30 @@
+#pragma once
+#include "cmd.cpp"
+#include "ssd_interface.h"
+#include "gmock/gmock.h"
+#include <string>
+#include <vector>
+
+class TestShell {
+public:
+    TestShell(SSD* ssd)
+        : ssd(ssd)
+    {
+    }
+    string read(uint32_t address);
+    string write(uint32_t address, string value);
+    string help();
+    string exit();
+    string fullWrite(string value);
+    string fullRead();
+    string erase(int address, int size);
+    bool isValidCommandAndArgument(string command);
+
+private:
+    SSD* ssd;
+    const int SSD_MINIMUM_ADDRESS = 0;
+    const int SSD_MAXIMUM_ADDRESS = 100;
+
+    bool isValidAddress(uint32_t address);
+    bool isValidValue(string value);
+};
